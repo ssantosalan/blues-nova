002f64684b5861447bbeb78aef91cb764604a12f
Alan Santos <ss.alan@outlook.com>
2021-04-22 17:37:17 -0300

    /sobre

---
+++ pages/sobre.js
@@ -0,0 +1,10 @@
+function Sobre() {
+    return (
+        <div>
+            <h1>Sobre</h1>
+            <a href="/">Acessar página Home</a>
+            <Contador />
+        </div>
+    )
+
+    export default Sobre
