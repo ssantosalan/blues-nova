9b13044db0cd5defa997359e8b60540f7786716b
Alan Santos <ss.alan@outlook.com>
2021-04-23 15:20:42 -0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,11 @@ function Home() {
             <h1>Blues Nova</h1>
 
             <p>Você pode acessar o meu <a href="https://github.com/ssantosalan" target="_blank" rel="external">GitHub</a></p>
-            <Link href="/sobre">
+            
             
             <p>Você pode acessar o meu <a href="https://www.linkedin.com/in/alan-s-santos/" target="_blank" rel="external">LinkedIn</a></p>
             
+            <Link href="/sobre">
             <a >Acessar página Sobre</a>
             </Link>
         </div>
