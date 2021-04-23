1be7ac15cd6f5d2bf61fcc0469a837e88c53bf51
Alan Santos <ss.alan@outlook.com>
2021-04-23 15:10:06 -0300

    github

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div>
             <h1>Blues Nova</h1>
-
+            <p>Você pode acessar o meu <a href="https://github.com/ssantosalan" target="_blank" rel="external">GitHub</a></p>
             <Link href="/sobre">
             <a >Acessar página Sobre</a>
             </Link>
