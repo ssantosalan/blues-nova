0538f1a6afdabfde10858ea16b26e3776f3cc23c
Alan Santos <ss.alan@outlook.com>
2021-04-23 15:16:51 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,13 @@ function Home() {
     return (
         <div>
             <h1>Blues Nova</h1>
+
             <p>Você pode acessar o meu <a href="https://github.com/ssantosalan" target="_blank" rel="external">GitHub</a></p>
             <Link href="/sobre">
-            <a >Acessar página Sobre</a>
+            
             <p>Você pode acessar o meu <a href="https://www.linkedin.com/in/alan-s-santos/" target="_blank" rel="external">LinkedIn</a></p>
+            
+            <a >Acessar página Sobre</a>
             </Link>
         </div>
     )
